3cefadcdf680299b6256656d94caeebb48cc5401
Dion Theroulde <dionjr105@gmail.com>
2021-10-21 16:56:33 -0400

    added nested form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @user.activities.build
   end
 
   private 
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,5 @@
 class Instructor < ApplicationRecord
   has_many :activities
   has_many :users, through: :activities
+
 end
